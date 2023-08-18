d15ee28179abf2762f175d9011797cf70d7f4190
Ramonlleopoldo <ramon.jl@outlook.com>
2023-08-18 19:48:33 -0300

    exercicio01

---
+++ POO/Exercicios/exercicio01.java
@@ -0,0 +1,17 @@
+package POO.Exercicios;
+
+public class exercicio01 {
+    byte idade = 10;
+
+    short ano = 2000;
+
+    long numero = 489853875;
+
+    double preço = 9.99;
+
+    boolean existe = true;
+
+    boolean naoexiste = false;
+
+    char sexo = 'F';
+}
