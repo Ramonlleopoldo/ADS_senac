c933643934278084bdaa6bbe344664db05aaec08
Ramonlleopoldo <ramon.jl@outlook.com>
2023-08-18 19:50:28 -0300

    exercicio01

---
+++ Porgramacao_POO/exercicio01.java
@@ -0,0 +1,19 @@
+package Porgramacao_POO;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        byte idade = 10;
+
+        short ano = 2000;
+
+        long numero = 489853875;
+
+        double preço = 9.99;
+
+        boolean existe = true;
+
+        boolean naoexiste = false;
+
+        char sexo = 'F';
+    }
+}
